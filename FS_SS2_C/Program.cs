a04e58836f4a4e1eda305807ad436742468d1837
Furji UMY <furji.ft21@mail.umy.ac.id>
2023-04-05 16:14:36 +0700

    add console

--- FS_SS2_C/Program.cs
+++ FS_SS2_C/Program.cs
@@ -11,6 +11,26 @@ namespace FS_SS2_C
         static void Main(string[] args)
         {
             Program pr = new Program();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Koneksi ke database\n");
+                    Console.WriteLine("masukkan User ID :");
+                    string user = Console.ReadLine();
+                    Console.WriteLine("Masukkan Password");
+                    string pass = Console.ReadLine();
+                    Console.WriteLine("Masukkan Database Tujuan :");
+                    string dtb = Console.ReadLine();
+                    Console.Write("\nKetik K untuk terhubung ke database");
+                    char chr = Convert.ToChar(Console.ReadLine());
+                    switch (chr)
+                    {
+
+                    }
+
+                }
+            }
         }
     }
 }
