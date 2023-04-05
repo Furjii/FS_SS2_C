e7b3e86022304664b43a6546783800e65afac616
Furji UMY <furji.ft21@mail.umy.ac.id>
2023-04-05 16:03:32 +0700

    Add project files.

---
+++ FS_SS2_C/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FS_SS2_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+        }
+    }
+}
